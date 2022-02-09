bf36877c2d35c917f5854f95f4f33f21e9ea2034
ethmoran <84296300+ethmoran@users.noreply.github.com>
2022-02-08 20:40:32 -0800

    javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,23 @@
+var img1=document.querySelector(".sagecover")
+var img2=document.querySelector(".stotlecover")
+var path1="./Images/sage cover.jpg"
+var path2="./Images/stotle cover.jpg"
 
-
+img1.addEventListener("click",function(){
+    if(img1.getAttribute("src")===path1){
+        img1.setAttribute("src", path2)
+        img2.setAttribute("src", path1)
+    }else{
+        img1.setAttribute("src", path1)
+        img2.setAttribute("src", path2)
+    }
+})
+img2.addEventListener("click",function(){
+    if(img1.getAttribute("src")===path1){
+        img1.setAttribute("src", path2)
+        img2.setAttribute("src", path1)
+    }else{
+        img1.setAttribute("src", path1)
+        img2.setAttribute("src", path2)
+    }
+})
